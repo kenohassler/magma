b8cff8339e82cce5c62fe0396d122deb2a5cf621
Keno Hassler <40292329+kenohassler@users.noreply.github.com>
2024-08-21 15:30:39 +0200

    fix angora

--- fuzzers/angora/preinstall.sh
+++ fuzzers/angora/preinstall.sh
@@ -2,12 +2,15 @@
 set -e
 
 apt-get update && \
-    apt-get install -y make build-essential cmake git golang-go \
+    apt-get install -y make build-essential cmake git \
     python-pip python-dev wget zlib1g-dev
 
 # Install Python packages
 pip install --upgrade pip==9.0.3
 pip install wllvm
 
+# install newer version of go (back-ported from 7da098fb)
+wget -qO- https://go.dev/dl/go1.19.1.linux-amd64.tar.gz | tar xz -C /usr/local/ --strip-components=1
+
 # Install gllvm
-go get github.com/SRI-CSL/gllvm/cmd/...
+go install github.com/SRI-CSL/gllvm/cmd/...@latest
